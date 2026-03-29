ae8d0fd625a1c1887a57d247d0fa309647ee413c
myntz4 <sunglee459@gmail.com>
2026-03-29 22:54:34 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const cacheName = 'sudoku-v1';
+const assets = [
+  './',
+  './Sudoku_App.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
